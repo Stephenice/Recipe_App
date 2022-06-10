b8fe54a39e8c1f477bf99b5be298e4bd70068ac9
stephenice <stephencode@hotmail.com>
2022-06-10 19:16:36 +0100

    Implementing Pagination

--- src/js/model.js
+++ src/js/model.js
@@ -1,12 +1,14 @@
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config';
 import { getJSON } from './helper';
+import { RES_PER_PAGE } from './config';
 
 export const state = {
   recipe: {},
   search: {
     query: '',
     results: [],
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -55,4 +57,10 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-// loadSearchResults('pizza');
+// pagniation
+export const getSearchResultsPage = function (page) {
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
+};
