b9e4480924bcbae8b261fc2e862fbe04a24a460a
stephenice <stephencode@hotmail.com>
2022-06-16 01:23:07 +0100

    Developed a DOM Updating Algorithm

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -21,7 +21,21 @@ export default class View {
     const newMarkup = this._generateMarkup();
 
     const newDOM = document.createRange().createContextualFragment(newMarkup);
-    const newElements = newDOM.querySelectorAll('*');
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl));
+
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        console.log(newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+    });
   }
 
   _clear() {
